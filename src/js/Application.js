e2dccdf0aa1ca71907043f1fcb86b51b952423d8
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-05 16:17:58 +0200

    add anime

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,16 +1,36 @@
 import EventEmitter from "eventemitter3";
 import anime from "animejs";
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this.init();
-    this.emit(Application.events.READY);
-  }
-  init() {}
+    constructor() {
+        super();
+        this.init();
+
+        this.emit(Application.events.READY);
+
+    }
+    init() {
+        let article = document.querySelector('.article');
+        article.addEventListener('click', (e) => {
+            console.log(article);
+
+            anime({
+                targets: '.article',
+                translateX: 250,
+                direction: 'alternate',
+                loop: true,
+                
+            })
+
+        })
+
+    }
+
+
+
 }
